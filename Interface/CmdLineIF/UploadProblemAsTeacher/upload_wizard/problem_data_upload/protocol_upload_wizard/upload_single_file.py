af1d6bc661f1230869f16aa0516e4790f3a5ff32
IgorZhiltsoff <zhiltsov.is@phystech.edu>
2022-04-10 19:44:18 +0300

    Replaced full paths with their suffixes "TPy_project/..."

--- Interface/CmdLineIF/UploadProblemAsTeacher/upload_wizard/problem_data_upload/protocol_upload_wizard/upload_single_file.py
+++ Interface/CmdLineIF/UploadProblemAsTeacher/upload_wizard/problem_data_upload/protocol_upload_wizard/upload_single_file.py
@@ -6,4 +6,6 @@ def upload_single_file(semantics, path_to_protocol_dir, key_seq_to_current_dict,
     file_src = input(f"Input paths to {semantics}: ")
     file_dest = f'{path_to_protocol_dir}/{short_semantics}'
     copy_file(file_src, file_dest)
-    custodian.nested_append(key_seq_to_current_dict + (f'{semantics}files',), file_dest)
+
+    suffix = file_dest[file_dest.find('TPy_project'):]
+    custodian.nested_append(key_seq_to_current_dict + (f'{semantics}files',), suffix)
