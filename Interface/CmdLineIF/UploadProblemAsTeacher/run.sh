633ab90f891eb072b02f0d9e8d7c3098f124b0f4
IgorZhiltsoff <zhiltsov.is@phystech.edu>
2022-04-12 01:53:00 +0300

    Added Makefile

--- Interface/CmdLineIF/UploadProblemAsTeacher/run.sh
+++ Interface/CmdLineIF/UploadProblemAsTeacher/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-pip install -r requirements.txt
-
 problems_base=$( readlink -f "/home/zhilts/PycharmProjects/TPy_project/problems_base" )
 PYTHONPATH=..:../DataCustodian python3 main_driver.py $problems_base 1
