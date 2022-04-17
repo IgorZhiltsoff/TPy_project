e5ac7c6d08031b177384cb2e522951b4662ac2e5
IgorZhiltsoff <zhiltsov.is@phystech.edu>
2022-04-17 21:30:06 +0300

    drew up root

--- Interface/ServerIF/server/server.py
+++ Interface/ServerIF/server/server.py
@@ -1,1 +1,32 @@
+import flask
+import sys
 
+address = sys.argv[0]
+port = sys.argv[1]
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def init():
+    return flask.render_template('root.html')
+
+
+@app.route('/display_readme')
+def display_readme():
+    return flask.render_template('display_readme.html')
+
+
+@app.route('/display_problems')
+def display_problems():
+    return flask.render_template('display_problems.html')
+
+
+@app.route('/submit')
+def submit():
+    pass
+
+
+@app.route('/upload_problem')
+def upload_problem():
+    pass
