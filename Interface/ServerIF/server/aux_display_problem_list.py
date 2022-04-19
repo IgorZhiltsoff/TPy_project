fa95fd26d91c95deb421467bfbca9043653f6ee7
IgorZhiltsoff <zhiltsov.is@phystech.edu>
2022-04-20 02:22:35 +0300

    Switched to Jinja!

--- Interface/ServerIF/server/aux_display_problem_list.py
+++ Interface/ServerIF/server/aux_display_problem_list.py
@@ -1,46 +1,12 @@
 import os
-from lxml import etree
+
+import flask
 from aux_display_problem import \
-    get_path_to_problem_base, get_problem_full_name, get_html_tree, \
-    append_child_to_body, prepend_child_to_body, generate_html_tag
+    get_path_to_problem_base, get_problem_full_name
 
 
 def display_problem_list():
     problem_ids = sorted(map(int, os.listdir(get_path_to_problem_base())))
-    tree = generate_list_skeleton_tree()
-    for problem_id in problem_ids:
-        paragraph = generate_problem_paragraph_link_tag(problem_id)
-        append_child_to_body(
-            new=paragraph,
-            tree=tree
-        )
-
-    append_child_to_body(
-        new=generate_back_to_main_page_link_tag(),
-        tree=tree
-    )
-
-    return etree.tostring(tree).decode('utf-8')
-
-
-def generate_list_skeleton_tree():
-    html = '<html><body><h1>List of uploaded problems:</h1></body></html>'
-    return get_html_tree(html)
-
-
-def generate_problem_paragraph_link_tag(problem_id):
-    paragraph = etree.Element('p')
-    etree.SubElement(
-        _parent=paragraph,
-        _tag='a',
-        href=f"display_problems?problem_id={problem_id}"
-    ).text = get_problem_full_name(problem_id)
-    return paragraph
-
-
-def generate_back_to_main_page_link_tag():
-    return generate_html_tag(
-        descriptor='a',
-        text='Back to main page',
-        href="/"
-    )
+    full_names = map(get_problem_full_name, problem_ids)
+    problems_info = list(zip(full_names, problem_ids))
+    return flask.render_template('display_problem_list.html', problems_info=problems_info)
