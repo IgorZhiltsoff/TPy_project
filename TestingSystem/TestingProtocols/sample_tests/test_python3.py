373a7ab64b2967b42b6017b4a61083e31c34d22b
IgorZhiltsoff <zhiltsov.is@phystech.edu>
2022-04-30 13:56:53 +0300

    Passed PYTHON tests

--- TestingSystem/TestingProtocols/sample_tests/test_python3.py
+++ TestingSystem/TestingProtocols/sample_tests/test_python3.py
@@ -4,6 +4,8 @@ from testing_protocols import InputOutput, InputCustomChecker, RandomInputCustom
     UserSubmittedData
 from language_support import python_data, LanguageLabel
 
+python_language_data_set = python_data.data_set
+
 accepted = UserSubmittedData('accepted.py', 1, LanguageLabel.PYTHON3)
 wrong_answer = UserSubmittedData('wrong_answer.py', 2, LanguageLabel.PYTHON3)
 runtime_error = UserSubmittedData('runtime_error.py', 3, LanguageLabel.PYTHON3)
@@ -20,7 +22,7 @@ class Python3AnswerTest(BaseTestCase):
             input_output_paths_dict={"inout_tests/1.in": "inout_tests/1.out",
                                      "inout_tests/2.in": "inout_tests/2.out",
                                      "inout_tests/3.in": "inout_tests/3.out"},
-            language_data_set=python_data.python3_data
+            language_data_set=python_language_data_set
         )
 
         user_submitted_data_to_expected_verdict = \
@@ -36,7 +38,7 @@ class Python3AnswerTest(BaseTestCase):
                              "in_custom_tests/2.in",
                              "in_custom_tests/3.in"},
             path_to_checker_exec='in_custom_tests/custom_checker.out',
-            language_data_set=python_data.python3_data
+            language_data_set=python_language_data_set
         )
 
         user_submitted_data_to_expected_verdict = \
@@ -51,7 +53,7 @@ class Python3AnswerTest(BaseTestCase):
             test_count=3,
             path_to_input_generation_exec='rand_custom_tests/random_generator.out',
             path_to_checker_exec='rand_custom_tests/custom_checker.out',
-            language_data_set=python_data.python3_data
+            language_data_set=python_language_data_set
         )
 
         user_submitted_data_to_expected_verdict = \
@@ -66,7 +68,7 @@ class Python3AnswerTest(BaseTestCase):
             path_to_header='limited_work_space/header.py',
             path_to_footer='limited_work_space/footer.py',
             extension='.py',
-            language_data_set=python_data.python3_data
+            language_data_set=python_language_data_set
         )
 
         limited_work_space_accepted = UserSubmittedData('limited_work_space/accepted.py', 1, LanguageLabel.PYTHON3)
